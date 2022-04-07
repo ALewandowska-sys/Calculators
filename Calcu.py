69a4bf25d46d01c813d779b7b0a543fd2a9bda2c
Aleksandra <82601472+ALewandowska-sys@users.noreply.github.com>
2022-04-07 22:21:25 +0200

    Create Calcu.py

---
+++ Calcu.py
@@ -0,0 +1,21 @@
+import operator
+
+listOfNumbers = []
+ops = {'+' : operator.add, '-' : operator.sub, '*' : operator.mul, '/' : operator.truediv, '%' : operator.mod, '^' : operator.xor,}
+
+
+print('Welcome to the EasyCalcu!\nEvery numbers and operation seperate by Enter \nUse E to close an aplication')
+
+while True:
+  a = input()
+  if a.isdigit():
+    listOfNumbers.append(int(a))
+  else:
+    if a == 'E':
+      break
+    else:
+      task = a
+  if len(listOfNumbers) == 2:
+    print('= ' , ops[task](listOfNumbers[0],listOfNumbers[1]))
+    listOfNumbers.clear()
+  
